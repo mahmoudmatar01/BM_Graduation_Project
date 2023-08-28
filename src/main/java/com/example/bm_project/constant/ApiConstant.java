863e18efc6a8ceb3a7a16ea97c55b81abfa7679b
mahmoudmatar <mahmoudmatar49@gmail.com>
2023-08-27 23:44:22 -0700

    add deploy annotation

--- src/main/java/com/example/bm_project/constant/ApiConstant.java
+++ src/main/java/com/example/bm_project/constant/ApiConstant.java
@@ -6,6 +6,7 @@ public class ApiConstant {
     public static final String ClientBASEURL ="https://v6.exchangerate-api.com/v6";
     public static final String MyApiBaseUrl="api/v1";
     public static final String ExchangeRateUrl=MyApiBaseUrl+"/exchange-rate";
+    public static final String CurrencyConversionUrl=MyApiBaseUrl+"/currencies-conversion";
     public static final String CurrenciesInfoUrl=MyApiBaseUrl+"/currencies-info";
     public static final String MultiCurrencyConversion=MyApiBaseUrl+"/currencies-compare-rate";
 
