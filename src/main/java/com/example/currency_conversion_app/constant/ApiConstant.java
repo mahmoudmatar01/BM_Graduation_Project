7d115386690a29bfc27a13539157e3f88ce8ceb2
FarahAbuelazm <FarahAbuelazm@gmail.com>
2023-08-29 16:56:30 +0300

    handle

--- src/main/java/com/example/currency_conversion_app/constant/ApiConstant.java
+++ src/main/java/com/example/currency_conversion_app/constant/ApiConstant.java
@@ -2,7 +2,7 @@ package com.example.currency_conversion_app.constant;
 
 
 public class ApiConstant {
-    public static final String ClientAPIKEY ="53ed5f323a9cdaac5055e13d";
+    public static final String ClientAPIKEY ="017e3225270654334afce908";
     public static final String ClientBASEURL ="https://v6.exchangerate-api.com/v6";
     public static final String MyApiBaseUrl="api/v1";
     public static final String ExchangeRateUrl=MyApiBaseUrl+"/exchange-rate";
